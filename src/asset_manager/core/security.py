d22fde48afebfd6393dca7a2de7294f6359f8bae
mikolaj-kopacz <mikolajk411@gmail.com>
2025-12-09 15:58:47 +0100

    add Token generation

--- src/asset_manager/core/security.py
+++ src/asset_manager/core/security.py
@@ -1,4 +1,8 @@
 from passlib.context import CryptContext
+from datetime import timedelta, timezone, datetime
+import jwt
+from src.asset_manager.core.config import settings
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +11,17 @@ def verify_password(plain_password, hashed_password) -> bool:
 
 def get_password_hash(password) -> str:
     return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    now = datetime.now(timezone.utc)
+
+    if expires_delta:
+        expire = now + expires_delta
+    else:
+        expire = now + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, settings.ALGORITHM)
+
+    return  encoded_jwt
