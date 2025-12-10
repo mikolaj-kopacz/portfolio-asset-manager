c8e50a20c6ba202dab45f9111b6dd3e9d1939dac
mikolaj-kopacz <mikolajk411@gmail.com>
2025-12-10 15:19:36 +0100

    add router

--- src/asset_manager/api/router.py
+++ src/asset_manager/api/router.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 from src.asset_manager.api.v1.endpoints import users
+from src.asset_manager.api.v1.endpoints import auth
 
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix="/users",tags=["users"])
+
+api_router.include_router(auth.router, prefix="",tags=["auth"])
