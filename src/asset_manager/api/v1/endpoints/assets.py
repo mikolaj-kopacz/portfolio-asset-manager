bdfbe4f174cb32a00f07a267baa7f377e35a786d
mikolaj-kopacz <mikolajk411@gmail.com>
2025-12-10 15:41:20 +0100

    fix create_asset

--- src/asset_manager/api/v1/endpoints/assets.py
+++ src/asset_manager/api/v1/endpoints/assets.py
@@ -10,7 +10,7 @@ router = APIRouter()
 
 @router.post("/",response_model=AssetRead)
 def create_asset(asset_in: AssetCreate,db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
-    asset = Asset.model_validate(asset_in)
+    asset = Asset(**asset_in.model_dump(),owner_id = current_user.id)
     asset.owner_id = current_user.id
     db.add(asset)
     db.commit()
